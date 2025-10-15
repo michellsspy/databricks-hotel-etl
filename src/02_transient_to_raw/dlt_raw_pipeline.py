c7cc776b19dffbd5729c36040e0dc4f7cde9d1c4
michellsspy <michellss.py@gmail.com>
2025-10-14 21:15:51 -0300

    Corrigir caminho do notebook no pipeline DLT

--- src/02_transient_to_raw/dlt_raw_pipeline.py
+++ src/02_transient_to_raw/dlt_raw_pipeline.py
@@ -27,30 +27,30 @@ def hospedes_raw():
 
 @dlt.table(
   name="consumos_raw",
-  comment="Cópia inicial da tabela de hospedes."
+  comment="Cópia inicial da tabela de consumos."
 )
 def consumos_raw():
-  return spark.read.table("production.transient.source_hospedes")
+  return spark.read.table("production.transient.source_consumos")
 
 @dlt.table(
   name="faturas_raw",
-  comment="Cópia inicial da tabela de hospedes."
+  comment="Cópia inicial da tabela de faturas."
 )
 def faturas_raw():
-  return spark.read.table("production.transient.source_hospedes")
+  return spark.read.table("production.transient.source_faturas")
 
 
 @dlt.table(
   name="reservas_raw",
-  comment="Cópia inicial da tabela de hospedes."
+  comment="Cópia inicial da tabela de reservas."
 )
 def reservas_raw():
-  return spark.read.table("production.transient.source_hospedes")
+  return spark.read.table("production.transient.source_reservas")
 
 
 @dlt.table(
   name="reservas_canal_raw",
-  comment="Cópia inicial da tabela de hospedes."
+  comment="Cópia inicial da tabela de reservas_canal."
 )
 def reservas_canal_raw():
-  return spark.read.table("production.transient.source_hospedes")
+  return spark.read.table("production.transient.source_reservas_canal")
